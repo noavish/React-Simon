0aa088075ab457995e2c47ffb64aa8e13be96ffb
noavish <noavish1@gmail.com>
2018-02-08 14:31:10 +0200

    state:

--- react-simon/src/Game.js
+++ react-simon/src/Game.js
@@ -29,27 +29,32 @@ class Game extends Component {
 
     playerClicked(e){
         let choise = e.target.dataset.id;
-        return choise;
+        this.setState({userChoose: this.state.userChoose.concat(choise)}, ()=> {
+            console.log(this.state.userChoose);
+        });
     }
 
 
     startGame(e) {
-        var sequence = [];
-        var score=0;
+        // var sequence = [];
+        // var score=0;
 
-            for (var i = 0; i < 2; i++) {
+            // for (var i = 0; i < 2; i++) {
             var rand = Math.ceil(Math.random() * 4); //adiing random between 1-4 to sequence
-            sequence = sequence.concat(rand);
-            this.computreTurn(sequence);
-            var result = this.playerTurn(sequence, e);
-            if (result == false) {
-                alert("game over");
-                return;
-            }
-            score++;
-        }
+            this.setState({sequence: this.state.sequence.concat(rand)}, ()=> {
+               console.log(this.state.sequence);
+            });
+            // sequence = sequence.concat(rand);
+            // this.computreTurn(sequence);
+            // var result = this.playerTurn(sequence, e);
+            // if (result == false) {
+            //     alert("game over");
+            //     return;
+            // }
+            // score++;
+        // }
 
-        console.log("you won")
+        // console.log("you won")
     }
 
     render() {
